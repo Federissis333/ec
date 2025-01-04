d50cee1f235cc84a389a6ca6f86792996d627038
Federissis333 <110787619+Federissis333@users.noreply.github.com>
2025-01-04 00:11:08 -0300

    Add files via upload

---
+++ src/commands/Message/Geral/avatar.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require("discord.js");
+const { request } = require("undici");
+
+module.exports = {
+    name: "Ver Avatar",
+    type: 3,
+    run: async (client, interaction, config) => {
+
+        const membro = interaction.targetMessage.author.id;
+
+        const { statusCode, headers, trailers, body } = await request(`https://japi.rest/discord/v1/user/${membro}`);
+        const user = await body.json();
+
+        const avatar = user.data.avatarURL + '?size=4096'
+
+        const row = new ActionRowBuilder()
+        const button = new ButtonBuilder()
+            .setLabel('Baixar Avatar')
+            .setStyle(5)
+
+        const embed = new EmbedBuilder()
+            .setColor(process.env.COLOR1)
+            .setAuthor({ name: `${user.data.tag}` })
+            .setFooter({ text: `${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL({ format: "png" })}` })
+            .setTimestamp()
+
+        if (avatar === 'null?size=4096') { interaction.reply({ content: `Não encontrei o avatar deste usuário.`, ephemeral: true }); button.setURL(process.env.SUPORTE).setDisabled(true) } else if (avatar === 'undefined?size=4096') { interaction.reply({ content: `Não encontrei o avatar deste usuário.`, ephemeral: true }); button.setURL(process.env.SUPORTE).setDisabled(true) } else { button.setURL(avatar); row.addComponents(button); embed.setImage(avatar); interaction.reply({ embeds: [embed], components: [row] }) }
+
+    },
+};
